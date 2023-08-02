b51ba80004c48e45a6eadb7e218fb0762309af30
Futa HIRAKOBA <korosuke613613@gmail.com>
2023-08-03 01:29:03 +0900

    feat: using terraform cloud

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = "~> 1.5.2"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "korosuke613"
+    workspaces {
+      name = "terraform-cloudflare"
+    }
+  }
 
   required_providers {
     cloudflare = {
